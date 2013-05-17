7f0e491ce304604c41c8a8efb8de5de79473f991
supersheep <supersheepxjy@gmail.com>
2013-05-17 23:18:33 +0800

    add user

--- scripts/user.js
+++ scripts/user.js
@@ -1,4 +1,21 @@
 
 define(function (require, exports, module) {
-    console.log('shop')
+
+    var paper = window.paper = Raphael("canvas");
+
+    function renderObject(obj){
+        var func = paper[obj.type];
+        var data = obj.data || {};
+        if(!func || typeof func != "function")return;
+        var obj = func.call(paper).attr(obj.attr||{});
+        // obj.data(data);
+        for(var k in data){
+            obj.data(k,data[k])
+        }
+    }
+
+    $.getJSON("data.json",function(json){
+        json.forEach(renderObject);
+    });
+
 })
