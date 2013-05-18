4156032698221a7b85ebc79f45c30dc7376fec70
supersheep <supersheepxjy@gmail.com>
2013-05-18 15:38:01 +0800

    global leak

--- scripts/tools/data.js
+++ scripts/tools/data.js
@@ -3,7 +3,8 @@ define(function(require, exports, module){
     var DataMaker = require("../maker/data");
     
     exports.name = "data";
-
+    var maker = null;
+    
     exports.init = function(paper){
         maker = new DataMaker(paper);
     }
