8910c93be18f2b6b0f91d37642a1257b19310c6a
island205 <island205@gmail.com>
2013-05-18 21:37:04 +0800

    relative path bug

--- scripts/config.js
+++ scripts/config.js
@@ -1,9 +1,9 @@
 seajs.config({
     alias:{
-        "jquery":"./components/jquery/jquery.min.js",
-        "event":"./scripts/lib/event.js",
-        "util":"./scripts/lib/util.js",
-        "shape":"./scripts/shape.js",
+        "jquery":"../components/jquery/jquery.min.js",
+        "event":"../scripts/lib/event.js",
+        "util":"../scripts/lib/util.js",
+        "shape":"../scripts/shape.js",
     }
 });
 
