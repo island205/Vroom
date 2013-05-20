a2843b004a76420fa7ec53e66389eb4aa1b7fcfa
island205 <island205@gmail.com>
2013-05-20 21:13:21 +0800

    merge user2.html to user.html

--- scripts/user.js
+++ scripts/user.js
@@ -1,27 +1,23 @@
 
 define(function (require, exports, module) {
 
-    var paper = Raphael("canvas",1118,600);
+    var paperC5 = Raphael("canvas-c5",1118,600);
+    var paperFlyHorse = Raphael("canvas-fly-horse",1118,600);
     require("jquery");
-    function renderObject(obj){
-        if(obj.type == "circle")return;
-        if(obj.attr.x == null)return;
-        if(obj.attr.x == 0)return;
-        var func = paper[obj.type];
-        var data = obj.data || {};
-        if(!func || typeof func != "function")return;
-        var obj = func.call(paper).attr(obj.attr||{});
-        // obj.data(data);
-        for(var k in data){
-            obj.data(k,data[k])
-        }
-    }
 
-    function getData(cb){
+    $('body').keydown(function (evt) {
+        if (evt.which == 27) {
+            $("#canvas-c5").hide();
+            $("#canvas-fly-horse").hide();
+        }
+    })
+
+    function getDataC5(cb){
         var data = $.getJSON("c5.json",function(json){
             cb(json);
-            $("body").on("click",function(e){
-                $("#canvas").show();
+            $(".mark-c5").on("click",function(e){
+                $("#canvas-c5").show();
+                $("#canvas-fly-horse").hide();
             });
             $("image").on("click",function(e){
                 if(this.attributes.href.value == "/design/artificial-learning-chair.png"){
@@ -70,9 +66,83 @@ define(function (require, exports, module) {
     }
 
 
-    getData(function(json){
-        json.forEach(renderObject);
+    getDataC5(function(json){
+        json.forEach(function (obj) {
+            if(obj.type == "circle")return;
+            if(obj.attr.x == null)return;
+            if(obj.attr.x == 0)return;
+            var func = paperC5[obj.type];
+            var data = obj.data || {};
+            if(!func || typeof func != "function")return;
+            var obj = func.call(paperC5).attr(obj.attr||{});
+            // obj.data(data);
+            for(var k in data){
+                obj.data(k,data[k])
+            }
+        });
     });
 
-    
+    function getDataFH(cb){
+        var data = $.getJSON("c6.json",function(json){
+            cb(json);
+            $(".mark-fly-horse").on("click",function(e){
+                $("#canvas-c5").hide();
+                $("#canvas-fly-horse").show();
+            });
+            $("image").on("click",function(e){
+                if(this.attributes.href.value == "/design/round-corner-table.png"){
+                    var random = new Date().getTime()%3;
+                    switch(random){
+                        case 0:
+                            alert("你好，已经被预订了");
+                            break;
+                        case 1:
+                            alert("您好，6人桌，需要预定请拨打电话：88001100");
+                            break;
+                        case 2:
+                            alert("您好，您已预定，如需退订，请拨打：88002200");
+                            break;
+                        default:
+                            break;
+                    }
+                    
+                }
+                else if(this.attributes.href.value == "/design/round-table.png"){
+                    var random2 = new Date().getTime()%3;
+                    switch(random2){
+                        case 0:
+                            alert("你好，已经被预订了");
+                            break;
+                        case 1:
+                            alert("您好，4人桌，需要预定请拨打电话：88001100");
+                            break;
+                        case 2:
+                            alert("您好，您已预定，如需退订，请拨打：88002200");
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                else{
+                    alert("请点桌子预定")
+                }
+            });
+        });
+    }
+
+    getDataFH(function(json){
+        json.forEach(function (obj) {
+            if(obj.type == "circle")return;
+            if(obj.attr.x == null)return;
+            if(obj.attr.x == 0)return;
+            var func =paperFlyHorse[obj.type];
+            var data = obj.data || {};
+            if(!func || typeof func != "function")return;
+            var obj = func.call(paperFlyHorse).attr(obj.attr||{});
+            // obj.data(data);
+            for(var k in data){
+                obj.data(k,data[k])
+            }
+        });
+    });
 })
